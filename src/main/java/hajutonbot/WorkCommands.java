1db100fa0fffb5764387e25e120eea3e6e1bf82d
tommi <->
2023-06-27 14:39:57 +0300

    changes

--- src/main/java/hajutonbot/WorkCommands.java
+++ src/main/java/hajutonbot/WorkCommands.java
@@ -6,7 +6,7 @@ public class WorkCommands {
 
     public static void addListeners(List<OnMessageReceived> list) {
         list.add((m) -> {
-            if (m.getChannel().getId().equals("1111320685239869483")) {
+            if (m.getChannel().getId().equals("asddsaasd")) {
                 m.getChannel().sendMessage(("moi")).queue();
             }
         });
